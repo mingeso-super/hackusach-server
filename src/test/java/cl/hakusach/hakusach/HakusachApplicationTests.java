9e4f79bb1d21bd04ff85c9376f7abf1b4bd6f644
Jonas Varas <jvaras@sentra.cl>
2018-10-17 18:24:12 -0300

    Proyecto vacio

---
+++ src/test/java/cl/hakusach/hakusach/HakusachApplicationTests.java
@@ -0,0 +1,16 @@
+package cl.hakusach.hakusach;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HakusachApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
