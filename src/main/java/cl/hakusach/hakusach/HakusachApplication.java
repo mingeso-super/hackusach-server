b276f00d8a139c169723e5b3016647fb0d71d94d
Jonas Varas <jvf890@gmail.com>
2018-10-20 18:15:58 -0300

    configuración de proyecto

--- src/main/java/cl/hakusach/hakusach/HakusachApplication.java
+++ src/main/java/cl/hakusach/hakusach/HakusachApplication.java
@@ -2,9 +2,16 @@ package cl.hakusach.hakusach;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HakusachApplication {
+public class HakusachApplication extends SpringBootServletInitializer  {
+
+	@Override
+  	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    	return application.sources(HakusachApplication.class);
+  	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HakusachApplication.class, args);
