86e972945950b45c240db097561bfc1d05cfeacf
Jonas Varas <jvf890@gmail.com>
2018-10-23 02:51:37 -0300

    lenguajes

---
+++ src/main/java/cl/hakusach/hakusach/models/Languages.java
@@ -0,0 +1,10 @@
+package cl.hakusach.hakusach.models;
+
+
+public class Languages {
+
+    public static final String C_LANG = "C";
+    public static final String JAVA_LANG = "JAVA";
+    public static final String PYTHON_LANG = "PYTHON";
+
+}
