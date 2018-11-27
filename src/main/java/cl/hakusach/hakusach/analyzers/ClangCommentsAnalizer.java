d2b2e220c2646c6e972382a0262d6f0cb3f51dd7
Jonás Varas <jvf890@Nyan>
2018-11-27 20:32:23 -0300

    Variables para CommentAnalizer

--- src/main/java/cl/hakusach/hakusach/analyzers/ClangCommentsAnalizer.java
+++ src/main/java/cl/hakusach/hakusach/analyzers/ClangCommentsAnalizer.java
@@ -1,17 +1,34 @@
 package cl.hakusach.hakusach.analyzers;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+
+import static cl.hakusach.hakusach.analyzers.Ctypes.*;
 
 public class ClangCommentsAnalizer implements Analyzer {
+
+    private static String NAME = "(\\w+)(\\d*)";
+    private static String SPACE = " ";
+
+    private static String CUSTOM_STRUCT = "("+STRUCT+")" + 
+                                          "("+SPACE+"+"+")"+
+                                          "("+NAME+")";
+
+    private static String DATA_TYPES = "(" + VOID + "|" + 
+                                            INT + "|" + 
+                                            LONG +  "|" + 
+                                            CHAR + "|" + 
+                                            DOUBLE + "|" + 
+                                            FLOAT + "|" + 
+                                            CUSTOM_STRUCT+ ")";;
+
+    private Pattern pattern = Pattern.compile("");
 
     @Override
     public List<Result> check(String code) {
         
-
-        
-
-
-
+        List<Result> results = new ArrayList<>();
 
         return null;
 	}
